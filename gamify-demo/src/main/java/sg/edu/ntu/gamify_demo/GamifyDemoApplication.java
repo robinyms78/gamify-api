ae4346ed625c2849989ab4dc8f0b002893d9cb83
Robin Yap <robinyms78@gmail.com>
2025-02-25 17:16:21 +0800

    Updated dependencies and tested app

--- gamify-demo/src/main/java/sg/edu/ntu/gamify_demo/GamifyDemoApplication.java
+++ gamify-demo/src/main/java/sg/edu/ntu/gamify_demo/GamifyDemoApplication.java
@@ -5,12 +5,20 @@ package sg.edu.ntu.gamify_demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @SpringBootApplication
+@RestController
 public class GamifyDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamifyDemoApplication.class, args);
 	}
 
+	@GetMapping("/home")
+	public String home() {
+		return "Hello World!";
+	}
 }
